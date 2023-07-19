ae8d0c378ef26cb2d85968cd1f4ba31432c1500f
jiyongnoh <njy99000@naver.com>
2023-07-19 17:30:14 +0900

    comment: login router 주석 추가

--- router/login.js
+++ router/login.js
@@ -4,8 +4,9 @@ const router = express.Router();
 const { loginController } = require("../controller/index");
 const { loginHandler } = loginController;
 
+// get 요청 처리. query 데이터 처리.
 router.get("/", loginHandler("get"));
-
+// post 요청 처리. body 데이터 처리.
 router.post("/", loginHandler("post"));
 
 // public 모듈화 - require(router/path)을 통해 인스턴스 생성 가능.
