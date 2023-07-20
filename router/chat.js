7343ac251c56e836543091deb3a4684858cbf9ae
jiyongnoh <njy99000@naver.com>
2023-07-20 14:46:28 +0900

    comment: 소켓 서버와 클라이언트에 주석 추가

--- router/chat.js
+++ router/chat.js
@@ -7,22 +7,21 @@ const socketIO = require("socket.io");
 const port = 4001;
 const io = socketIO(server, {
   // 소켓 cors 처리
-  cors: {
-    // path를 제외한 host만 적어야 함.
-    origin: "http://127.0.0.1:5500",
-  },
+  cors: { origin: "*" },
 });
-
+// 소켓 연결 이벤트. 연결 발생 시 콜백 실행
 io.on("connection", (socket) => {
   console.log("연결 완료");
-
+  // msg 이벤트.
   socket.on("msg", (data) => {
+    // 소켓에 연결된 모든 client에게 msg 트리거를 발생시키고 data를 전달.
     io.emit("msg", data);
   });
 });
 
+// 소켓 서버 실행. (app.js의 app과 다른 서버이므로 따로 실행해야한다)
 server.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Socket server listening on port ${port}`);
 });
 
 // public 모듈화 - require(router/path)을 통해 인스턴스 생성 가능.
--- src/index.js
+++ src/index.js
@@ -42,35 +42,42 @@ const loginHandler = () => {
 };
 
 // 소켓 연결
-const socket = io(END_POINT);
+const socket = io.connect(END_POINT);
 
-// 소켓 서버 메시지 receive
+// msg 이벤트 등록. 서버의 msg 트리거 발동 시 실행. (receive)
 socket.on("msg", (data) => {
   const { nickname, msg, time } = data;
   const wrapper = document.querySelector(".wrapper");
+  // div 엘리먼트 생성
   const $chatting = document.createElement("div");
   // 다른 사용자의 메시지일 경우 색상 변경
   const myName = document.querySelector("#nickname").value;
   if (nickname !== myName) $chatting.style.color = "red";
 
+  // div 엘리먼트에 소켓 서버로부터 받은 메시지 입력
   $chatting.innerText = `${nickname}: ${msg} (${time})`;
+
+  // div 엘리먼트를 wrapper의 자식에 추가
   wrapper.appendChild($chatting);
 });
 
-// 소켓 서버 메시지 send
+// 메시지 send 핸들러.
 const msgHandler = () => {
   const msg = document.querySelector("#chat").value;
+  // 채팅 메시지가 있을 경우만 실행
   if (msg) {
     const nickname = document.querySelector("#nickname").value;
     const date = new Date();
     const time = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
 
+    // 소켓 서버에 msg 트리거 발생 및 데이터 전달.
     socket.emit("msg", {
       nickname,
       msg,
       time,
     });
 
+    // 채팅 메시지 input 초기화
     document.querySelector("#chat").value = "";
   }
 };
