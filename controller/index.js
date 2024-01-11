8348c9d77d0ea8ca8a322b7130600059561c03ee
노지용 <115959559+jiyongnoh@users.noreply.github.com>
2024-01-11 10:38:42 +0900

    feat: GPT 4 Turbo 모델 추가

--- controller/index.js
+++ controller/index.js
@@ -779,7 +779,7 @@ const openAIController = {
           },
           ...parseMessageArr,
         ],
-        model: "gpt-3.5-turbo", // gpt-3.5-turbo, gpt-4-0613, ft:gpt-3.5-turbo-1106:personal::8fIksWK3
+        model: "gpt-3.5-turbo", // gpt-4-1106-preview, gpt-3.5-turbo, ft:gpt-3.5-turbo-1106:personal::8fIksWK3
       });
 
       // console.log(response.choices[0]);
