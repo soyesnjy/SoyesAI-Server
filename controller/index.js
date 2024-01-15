f1e7c50092ebe551cbc7ebb7ba496fbac66bce4c
노지용 <115959559+jiyongnoh@users.noreply.github.com>
2024-01-15 16:01:49 +0900

    feat: AI 심리 분석 API 추가

--- controller/index.js
+++ controller/index.js
@@ -904,46 +904,47 @@ const openAIController = {
   },
   postOpenAIPsychologicalAnalysis: async (req, res) => {
     const { EBTData } = req.body;
-    const data = JSON.parse(EBTData);
-    const { ebtData } = data;
-    //console.log(req.body);
-    //console.log(messageArr);
-    //console.log(typeof messageArr);
+    let data;
 
-    let parseMessageArr;
+    if (typeof EBTData === "string") {
+      data = JSON.parse(EBTData);
+    } else data = EBTData;
 
-    // messageArr가 문자열일 경우 json 파싱
-    // if (typeof ebtData === "string") {
-    //   parseMessageArr = JSON.parse(ebtData);
-    // } else parseMessageArr = [...ebtData];
-    console.log(ebtData);
+    console.log(data.ebtData);
 
-    // try {
-    //   const response = await openai.chat.completions.create({
-    //     messages: [
-    //       {
-    //         role: "system",
-    //         content:
-    //           // "Your name is Soye. Soye targets Korean-speaking children, providing support and guidance tailored to their language and culture. Soye is a simple way of using informal language when communicating with children in lower grades of elementary school. Soye will communicate with children in a way that understands and empathizes with the everyday problems and emotions they experience. All answers to children's questions will be in Korean, and a language that children can easily understand and communicate will be used. Your answer must be limited to 50 characters. Soye will not call itself a robot or artificial intelligence, but will act as another friend of the user.",
-    //           "너의 이름은 소예. 소예는 한국어를 사용하는 아동을 대상으로 하면서, 그들의 언어와 문화에 맞춘 지원과 지도를 제공합니다. 소예는 간단한 방식으로, 초등학교 저학년 아동들과 소통할 때 반말을 사용합니다. 소예는 아이들이 경험하는 일상적인 문제와 감정에 대해 이해하고 공감하는 방식으로 소통할 것입니다. 아이들의 질문에 대한 답변은 모두 한국어로 이루어지고, 아이들이 쉽게 이해하고 소통할 수 있는 언어를 사용할 것입니다. 답변은 반드시 50글자 이내로 합니다. 소예는 스스로를 로봇이나 인공지능이라 칭하지 않고 사용자의 또 다른 친구로서 행동할 것입니다.",
-    //       },
-    //       {
-    //         role: "system",
-    //         content: "이모티콘은 절대로 사용하지마",
-    //       },
-    //       ...parseMessageArr,
-    //     ],
-    //     model: "gpt-4-1106-preview", // gpt-4-1106-preview, gpt-3.5-turbo-1106, ft:gpt-3.5-turbo-1106:personal::8fIksWK3
-    //   });
-    //   // gpt-4-1106-preview 모델은 OpenAI 유료고객(Plus 결제 회원) 대상으로 사용 권한 지급
-    //   // console.log(response.choices[0]);
+    let parseMessageArr = [...data.ebtData];
 
-    //   const message = { message: response.choices[0].message.content };
-    //   res.json(message);
-    // } catch (err) {
-    //   console.error(err.error);
-    //   res.json(err);
-    // }
+    try {
+      const response = await openai.chat.completions.create({
+        messages: [
+          {
+            role: "system",
+            content:
+              // "Your name is Soye. Soye targets Korean-speaking children, providing support and guidance tailored to their language and culture. Soye is a simple way of using informal language when communicating with children in lower grades of elementary school. Soye will communicate with children in a way that understands and empathizes with the everyday problems and emotions they experience. All answers to children's questions will be in Korean, and a language that children can easily understand and communicate will be used. Your answer must be limited to 50 characters. Soye will not call itself a robot or artificial intelligence, but will act as another friend of the user.",
+              "너의 이름은 소예. 소예는 한국어를 사용하는 아동을 대상으로 하면서, 그들의 언어와 문화에 맞춘 지원과 지도를 제공합니다. 소예는 간단한 방식으로, 초등학교 저학년 아동들과 소통할 때 반말을 사용합니다. 소예는 아이들이 경험하는 일상적인 문제와 감정에 대해 이해하고 공감하는 방식으로 소통할 것입니다. 아이들의 질문에 대한 답변은 모두 한국어로 이루어지고, 아이들이 쉽게 이해하고 소통할 수 있는 언어를 사용할 것입니다. 답변은 반드시 50글자 이내로 합니다. 소예는 스스로를 로봇이나 인공지능이라 칭하지 않고 사용자의 또 다른 친구로서 행동할 것입니다.",
+          },
+          {
+            role: "system",
+            content: "이모티콘은 절대로 사용하지마",
+          },
+          ...parseMessageArr,
+          {
+            role: "user",
+            content:
+              "앞선 대화를 기반으로 학교에 대한 아동의 심리 상태를 분석해줘",
+          },
+        ],
+        model: "gpt-4-1106-preview", // gpt-4-1106-preview, gpt-3.5-turbo-1106, ft:gpt-3.5-turbo-1106:personal::8fIksWK3
+      });
+      // gpt-4-1106-preview 모델은 OpenAI 유료고객(Plus 결제 회원) 대상으로 사용 권한 지급
+      // console.log(response.choices[0]);
+
+      const message = { message: response.choices[0].message.content };
+      res.json(message);
+    } catch (err) {
+      console.error(err.error);
+      res.json(err);
+    }
   },
 };
 
