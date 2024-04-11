79941d71648cb1e7a80439b6e70213163e20019e
노지용 <115959559+jiyongnoh@users.noreply.github.com>
2024-04-11 10:50:19 +0900

    feat: SignUp API 추가. 현재 회원가입 진행 시 id, pwd만 입력 시 진행 가능하도록 설정.

--- router/signup.js
+++ router/signup.js
@@ -2,15 +2,18 @@
 const express = require("express");
 const router = express.Router();
 const { errController } = require("../controller/index");
-const { signupController } = require("../controller/signup");
-const { dupleCheckHandler, signupHandler } = signupController;
+const {
+  signupController,
+  signupController_Regercy,
+} = require("../controller/signup");
+const { postSignupAIHandler } = signupController;
+const { dupleCheckHandler, signupHandler } = signupController_Regercy;
 
 router.post("/", signupHandler);
 router.post("/duplecheck", dupleCheckHandler);
 
 // AI 프로젝트용 회원가입 컨트롤러
-// router.post("/ai");
-// router.post("/ai/duplecheck");
+router.post("/ai", postSignupAIHandler);
 
 // 에러 메세지 처리
 router.use(errController.errMessageHandler);
