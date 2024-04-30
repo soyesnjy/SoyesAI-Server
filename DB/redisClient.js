5b528940bb979c479f8e8af8e66c9b259a331340
노지용 <115959559+jiyongnoh@users.noreply.github.com>
2024-04-30 10:02:04 +0900

    rollback: redis 접근 URL 롤백

--- DB/redisClient.js
+++ DB/redisClient.js
@@ -3,10 +3,7 @@ require("dotenv").config();
 const redis = require("redis");
 const RedisStore = require("connect-redis").default;
 const redisClient = redis.createClient({
-  url:
-    process.env.DEV_OPS === "local"
-      ? "redis://soyeskids.co.kr:6379"
-      : "redis://127.0.0.1:6379",
+  url: "redis://soyeskids.co.kr:6379",
 });
 
 redisClient.connect().catch(console.error);
