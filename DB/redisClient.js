1c714e2ee404d1ebc50b8ad14f73f597bf574ba3
노지용 <115959559+jiyongnoh@users.noreply.github.com>
2024-05-21 12:21:08 +0900

    fix: redis url 수정

--- DB/redisClient.js
+++ DB/redisClient.js
@@ -2,11 +2,16 @@ require("dotenv").config();
 
 const redis = require("redis");
 const RedisStore = require("connect-redis").default;
+
+// const redisClient = redis.createClient({
+//   url:
+//     process.env.DEV_OPS === "local"
+//       ? "redis://soyeskids.co.kr:6379"
+//       : "redis://172.18.0.3:6379", // docker network IPv4 Address
+// });
+
 const redisClient = redis.createClient({
-  url:
-    process.env.DEV_OPS === "local"
-      ? "redis://soyeskids.co.kr:6379"
-      : "redis://172.18.0.3:6379", // docker network IPv4 Address
+  url: "redis://soyeskids.co.kr:6379",
 });
 
 redisClient.connect().catch(console.error);
