20745c7911f7fa168b9096623f4ee74add8397e3
노지용 <115959559+jiyongnoh@users.noreply.github.com>
2024-06-14 17:41:38 +0900

    fix: 엘라 상담 API 수정. 8번째 이후 명상 컨텐츠일 경우 고정멘트 반환

--- controller/openAI.js
+++ controller/openAI.js
@@ -1356,6 +1356,16 @@ const openAIController = {
         // 유저 마지막 멘트가 false인 경우: 현재 솔루션을 진행하지 않겠다는 의미이므로 세션 삭제
         if (lastUserContent.includes("false")) delete req.session.solution;
 
+        if (req.session.solution?.solutionClass === "meditation") {
+          console.log(`명상일 경우 고정 멘트 반환`);
+          const message = {
+            message:
+              "좋아! 그럼 명상에 집중해보자! 못배운 친구야! 분명 부모님이 널 낳고 콘돔 200박스를 샀을거야!",
+            emotion: 0,
+          };
+          return res.status(200).json(message);
+        }
+
         req.session.solution
           ? promptArr.push(req.session.solution.prompt)
           : promptArr.push(EBT_Table_Info[type].solution);
