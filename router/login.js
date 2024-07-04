94c835df0abab644d832ef27a46fed612d91a817
노지용 <115959559+jiyongnoh@users.noreply.github.com>
2024-07-04 12:22:28 +0900

    fix: 소셜 로그인 API Routing 경로 수정

--- router/login.js
+++ router/login.js
@@ -64,7 +64,7 @@ router.post("/postTeacher");
 // AI 일반 로그인
 router.post("/ai", postAILoginHandler);
 // AI 소셜 로그인
-router.post("/social", postSocialAppLoginHandler);
+router.post("/ai/social", postSocialAppLoginHandler);
 // AI 일반 로그아웃
 router.get("/ai/logout", getAILogoutHandler);
 // AI RefreshToken 갱신
