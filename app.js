1d9646e1b0175394ee2f847288475eb9d3e3fa07
노지용 <115959559+jiyongnoh@users.noreply.github.com>
2024-08-26 13:50:45 +0900

    comment: Jenkins Test용 주석 추가3

--- app.js
+++ app.js
@@ -95,6 +95,7 @@ app.set("views", path.join(__dirname, "/views"));
 app.get("/", (req, res) => {
   // Jenkins Test용 주석
   // Jenkins Test용 주석2
+  // Jenkins Test용 주석3
   res.status(200).json({ text: "Hello World!" });
 
   // res.render("index", {
