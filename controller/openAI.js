388f75b558857fea89a0122327511108e15f29ce
노지용 <115959559+jiyongnoh@users.noreply.github.com>
2024-09-11 13:05:37 +0900

    feat: 마루 게임 API 수정. 밸런스게임 프롬프트 수정. 매칭률 추가

--- controller/openAI.js
+++ controller/openAI.js
@@ -1661,14 +1661,15 @@ const openAIController = {
       if (game === "remarks") {
         promptArr.push({
           role: "system",
-          content: `assistant는 user와 끝말잇기 게임을 진행한다. 단어는 2 ~ 5글자 사이의 명사만으로 생성한다. 
+          content: `assistant는 user와 끝말잇기 게임을 진행한다. 단어는 2 ~ 5글자 사이의 명사만으로 생성한다.
 '륨', '릇'과 같은 한방단어로 인해 assistant가 패배할 경우 assistant는 패배를 인정하고 재시작 여부를 user에게 묻는다.`,
         });
       } else if (game === "balance") {
         promptArr.push({
           role: "system",
-          content: `assistant는 user와 밸런스 게임을 진행한다. 총 10회 질문한다. 
-assistant는 user의 응답에 반응하지 않고 반드시 밸런스게임 문제만 출제한다`,
+          content: `assistant는 user와 밸런스 게임을 진행한다. 총 10회 질문한다.
+assistant는 user의 응답에 반응하지 않고 반드시 밸런스게임 문제만 출제한다.
+게임이 종료되면 assistant가 선택한 단어가 나오고 user와의 매칭률을 계산해서 %로 알려준다. `,
         });
       }
 
