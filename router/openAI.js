06b28f484565cebded0104ddb4007dabf156b21a
노지용 <115959559+jiyongnoh@users.noreply.github.com>
2024-10-31 13:34:55 +0900

    feat: 결과보고서 API 인증 미들웨어 추가

--- router/openAI.js
+++ router/openAI.js
@@ -268,11 +268,7 @@ router.post(
 );
 
 // 결과보고서 PDF 이메일 발송
-router.post(
-  "/report",
-  // vaildateTokenConsulting,
-  postReportTest
-);
+router.post("/report", vaildateTokenConsulting, postReportTest);
 
 // 달력 데이터 반환
 router.post("/calendar", vaildateTokenConsulting, postOpenAIMypageCalendarData);
