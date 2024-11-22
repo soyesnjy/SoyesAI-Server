6ef3eff8b2d9b0e90188876edf5d306dcb853284
노지용 <115959559+jiyongnoh@users.noreply.github.com>
2024-11-22 10:27:29 +0900

    feat: 이용권 갱신 API 수정. 이용권 쿠폰 처리 추가 + feat: 쿠폰 유효성 검증 API 수정. 이용권 쿠폰 사용 로직 제거

--- controller/payment.js
+++ controller/payment.js
@@ -130,28 +130,30 @@ const PaymentController = {
         parseData = JSON.parse(data);
       } else parseData = data;
 
-      const { pUid, type, coupon_number, coupon_id, receipt } = parseData;
+      const { pUid, type, receipt, coupon_number, coupon_id } = parseData;
 
       console.log(`유저 이용권 만료일 갱신 API 호출 - pUid: ${pUid}`);
       console.log(parseData);
 
       // No Required => return
-      if (!pUid || !type || !receipt) {
+      if (!pUid) {
         console.log(`No Required input value - 400 - pUid: ${pUid}`);
         return res
           .status(400)
           .json({ message: "No Required input value - 400" });
       }
 
-      // No type matching => return
-      if (typeRegex.test(String(type))) {
-        console.log(`Type is not matching [day, month, year] - pUid: ${pUid}`);
+      // 결제 관련 변수 중 하나만 온 경우 => return (올거면 두 개 다와야 함)
+      if (Boolean(type) !== Boolean(receipt)) {
+        console.log(
+          `Only one of the payment-related variables was entered - pUid: ${pUid}`
+        );
         return res.status(400).json({
-          message: "Type is not matching [day, month, year]",
+          message: "Only one of the payment-related variables was entered",
         });
       }
 
-      // 두 변수 중 하나만 온 경우 => return
+      // 쿠폰 관련 변수 중 하나만 온 경우 => return (올거면 두 개 다와야 함)
       if (Boolean(coupon_number) !== Boolean(coupon_id)) {
         console.log(
           `Only one of the coupon-related variables was entered - pUid: ${pUid}`
@@ -161,31 +163,54 @@ const PaymentController = {
         });
       }
 
+      // No type matching => return
+      if (type && typeRegex.test(String(type))) {
+        console.log(`Type is not matching [day, month, year] - pUid: ${pUid}`);
+        return res.status(400).json({
+          message: "Type is not matching [day, month, year]",
+        });
+      }
+
       parsepUid = pUid;
 
-      const coupon_table = Subscription_Table_Info["Coupon"].table;
-      const log_table = Subscription_Table_Info["Log"].table;
+      const table = Subscription_Table_Info["Subscription"].table; // 유저 이용권 테이블
+      const coupon_table = Subscription_Table_Info["Coupon"].table; // 쿠폰 테이블
+      const log_table = Subscription_Table_Info["Log"].table; // 유저 이용권 로그 테이블
+
+      let coupon_select_data;
+
       // 해당 쿠폰 유효성 검증 및 사용 이력 조회
       if (coupon_id && coupon_number) {
         // 쿠폰 유효성 검증
         const select_query = `SELECT
-        coupon_type,
-        coupon_number
+        *
         FROM ${coupon_table}
         WHERE coupon_id='${coupon_id}'`;
 
-        const select_data = await fetchUserData(connection_AI, select_query);
+        coupon_select_data = await fetchUserData(connection_AI, select_query);
 
         if (
-          !select_data.length || // 없는 쿠폰
-          select_data[0]?.coupon_type !== "sale" || // 할인권 쿠폰이 아닐 경우
-          select_data[0]?.coupon_number !== coupon_number // 쿠폰 번호와 일치하지 않음
+          !coupon_select_data.length || // 없는 쿠폰
+          coupon_select_data[0]?.coupon_number !== coupon_number // 쿠폰 번호와 일치하지 않음
         ) {
           console.log(`Not Vaildation Coupon - pUid: ${pUid}`);
           return res.status(400).json({
             message: "유효한 쿠폰이 아닙니다 (Not Vaildation Coupon)",
           });
         }
+
+        // 쿠폰 타입이 subscription 인데 결제 관련 데이터가 들어온 경우
+        if (
+          coupon_select_data[0]?.coupon_type === "subscription" &&
+          (type || receipt)
+        ) {
+          console.log(`Not Vaildation Coupon - pUid: ${pUid}`);
+          return res.status(400).json({
+            message:
+              "유효한 결제 방식이 아닙니다 (There are input values that are not required when using a subscription coupon)",
+          });
+        }
+
         // 사용 이력 조회
         const log_select_query = `SELECT
         subscription_log_id,
@@ -208,9 +233,7 @@ const PaymentController = {
         }
       }
 
-      // 1. SELECT (쿠폰 row 확인)
-      const table = Subscription_Table_Info["Subscription"].table;
-
+      // 유저 이용권 테이블 SELECT
       const select_query = `SELECT * FROM ${table} WHERE uid='${parsepUid}'`;
       const select_data = await fetchUserData(connection_AI, select_query);
 
@@ -218,20 +241,32 @@ const PaymentController = {
       let sub_select_data = select_data;
 
       const today = moment().tz("Asia/Seoul").format("YYYY-MM-DD HH:mm:ss");
-      // 만료일이 지났는지 여부 확인
+      const subscription_period =
+        coupon_select_data &&
+        coupon_select_data[0]?.coupon_type === "subscription"
+          ? coupon_select_data[0]?.coupon_subscription_period
+          : typeMap[type]; // 갱신 기간 (1, 30, 365, [coupon_subscription_period])
+
+      // 기존 만료일 체크 및 만료일 갱신
       let expirationDate_value = sub_select_data[0]
         ?.subscription_expiration_date
         ? new Date(sub_select_data[0]?.subscription_expiration_date) <
           new Date(today)
-          ? addDays(typeMap[type]).toISOString().slice(0, 19).replace("T", " ")
+          ? addDays(subscription_period)
+              .toISOString()
+              .slice(0, 19)
+              .replace("T", " ")
           : addDays(
-              typeMap[type],
+              subscription_period,
               sub_select_data[0]?.subscription_expiration_date
             )
               .toISOString()
               .slice(0, 19)
               .replace("T", " ")
-        : addDays(typeMap[type]).toISOString().slice(0, 19).replace("T", " ");
+        : addDays(subscription_period)
+            .toISOString()
+            .slice(0, 19)
+            .replace("T", " ");
 
       // DB에 Row가 없을 경우 INSERT, 있으면 지정한 속성만 UPDATE
       const duple_query = `INSERT INTO ${table} 
@@ -297,17 +332,19 @@ const PaymentController = {
 
       try {
         // 구매 로그 삽입
-        await queryAsync(
-          connection_AI,
-          log_insert_query_purchase,
-          log_insert_value_purchase
-        );
-        console.log(
-          `Subscription Log Insert Success (Purchase)! - pUid: ${parsepUid}`
-        );
+        if (type && receipt) {
+          await queryAsync(
+            connection_AI,
+            log_insert_query_purchase,
+            log_insert_value_purchase
+          );
+          console.log(
+            `Subscription Log Insert Success (Purchase)! - pUid: ${parsepUid}`
+          );
+        }
 
-        // 쿠폰 로그 삽입 (조건 확인)
-        if (coupon_number) {
+        // 쿠폰 로그 삽입
+        if (coupon_id && coupon_number) {
           await queryAsync(
             connection_AI,
             log_insert_query_coupon,
@@ -422,101 +459,116 @@ const PaymentController = {
         });
       }
 
+      // if (coupon_type === "subscription") {
+      //   // 이용권 만료일 확인
+      //   const sub_table = Subscription_Table_Info["Subscription"].table;
+
+      //   const sub_select_query = `SELECT * FROM ${sub_table} WHERE uid='${parsepUid}'`;
+      //   const sub_select_data = await fetchUserData(
+      //     connection_AI,
+      //     sub_select_query
+      //   );
+
+      //   const today = moment().tz("Asia/Seoul").format("YYYY-MM-DD HH:mm:ss");
+      //   // expirationDate 지났는지 여부 확인
+      //   let expirationDate_value = sub_select_data[0]
+      //     ?.subscription_expiration_date
+      //     ? new Date(sub_select_data[0]?.subscription_expiration_date) <
+      //       new Date(today)
+      //       ? addDays(coupon_subscription_period)
+      //           .toISOString()
+      //           .slice(0, 19)
+      //           .replace("T", " ")
+      //       : addDays(
+      //           coupon_subscription_period,
+      //           sub_select_data[0]?.subscription_expiration_date
+      //         )
+      //           .toISOString()
+      //           .slice(0, 19)
+      //           .replace("T", " ")
+      //     : addDays(coupon_subscription_period)
+      //         .toISOString()
+      //         .slice(0, 19)
+      //         .replace("T", " ");
+
+      //   // DB에 Row가 없을 경우 INSERT, 있으면 지정한 속성만 UPDATE
+      //   const duple_query = `INSERT INTO ${sub_table}
+      //   (uid,
+      //   subscription_expiration_date,
+      //   subscription_status)
+      //   VALUES (?, ?, ?)
+      //   ON DUPLICATE KEY UPDATE
+      //   subscription_expiration_date = VALUES(subscription_expiration_date),
+      //   subscription_status = VALUES(subscription_status);`;
+
+      //   const duple_value = [parsepUid, expirationDate_value, "active"];
+
+      //   try {
+      //     await queryAsync(connection_AI, duple_query, duple_value);
+      //     console.log(
+      //       `User Subscription Expiration Date Update Success! - pUid: ${parsepUid}`
+      //     );
+      //     res.status(200).json({
+      //       message:
+      //         "이용권 쿠폰 사용에 성공했습니다! (User Coupon validation Success)",
+      //       expirationDate: expirationDate_value,
+      //     });
+      //   } catch (err) {
+      //     console.error("Error executing query:", err);
+      //     return res.status(500).json({
+      //       message: `Server Error: ${err.sqlMessage}`,
+      //     });
+      //   }
+      //   // Log Insert 처리
+      //   if (true) {
+      //     const log_insert_query = `INSERT INTO ${log_table} (
+      //     uid,
+      //     subscription_log_type,
+      //     subscription_log_coupon_number,
+      //     subscription_log_coupon_id
+      //     ) VALUES (?, ?, ?, ?)`;
+      //     // console.log(plan_log_insert_query);
+      //     const log_insert_value = [
+      //       parsepUid,
+      //       "coupon",
+      //       couponNumber,
+      //       coupon_id,
+      //     ];
+      //     // console.log(plan_log_insert_value);
+      //     try {
+      //       await queryAsync(connection_AI, log_insert_query, log_insert_value);
+      //       console.log(`Subscription Log Insert Success! - pUid: ${pUid}`);
+      //       return;
+      //     } catch (err) {
+      //       console.error("Error executing query:", err);
+      //       return res.status(500).json({
+      //         message: `Server Error: ${err.sqlMessage}`,
+      //       });
+      //     }
+      //   }
+      // }
+
       // 이용권인 경우
       if (coupon_type === "subscription") {
-        // 이용권 만료일 확인
-        const sub_table = Subscription_Table_Info["Subscription"].table;
-
-        const sub_select_query = `SELECT * FROM ${sub_table} WHERE uid='${parsepUid}'`;
-        const sub_select_data = await fetchUserData(
-          connection_AI,
-          sub_select_query
+        console.log(
+          `이용권 쿠폰 유효성 검증에 성공했습니다! (User Coupon Validation Success) - pUid: ${pUid}`
         );
-
-        const today = moment().tz("Asia/Seoul").format("YYYY-MM-DD HH:mm:ss");
-        // expirationDate 지났는지 여부 확인
-        let expirationDate_value = sub_select_data[0]
-          ?.subscription_expiration_date
-          ? new Date(sub_select_data[0]?.subscription_expiration_date) <
-            new Date(today)
-            ? addDays(coupon_subscription_period)
-                .toISOString()
-                .slice(0, 19)
-                .replace("T", " ")
-            : addDays(
-                coupon_subscription_period,
-                sub_select_data[0]?.subscription_expiration_date
-              )
-                .toISOString()
-                .slice(0, 19)
-                .replace("T", " ")
-          : addDays(coupon_subscription_period)
-              .toISOString()
-              .slice(0, 19)
-              .replace("T", " ");
-
-        // DB에 Row가 없을 경우 INSERT, 있으면 지정한 속성만 UPDATE
-        const duple_query = `INSERT INTO ${sub_table} 
-        (uid,
-        subscription_expiration_date,
-        subscription_status)
-        VALUES (?, ?, ?)
-        ON DUPLICATE KEY UPDATE
-        subscription_expiration_date = VALUES(subscription_expiration_date),
-        subscription_status = VALUES(subscription_status);`;
-
-        const duple_value = [parsepUid, expirationDate_value, "active"];
-
-        try {
-          await queryAsync(connection_AI, duple_query, duple_value);
-          console.log(
-            `User Subscription Expiration Date Update Success! - pUid: ${parsepUid}`
-          );
-          res.status(200).json({
-            message:
-              "이용권 쿠폰 사용에 성공했습니다! (User Coupon validation Success)",
-            expirationDate: expirationDate_value,
-          });
-        } catch (err) {
-          console.error("Error executing query:", err);
-          return res.status(500).json({
-            message: `Server Error: ${err.sqlMessage}`,
-          });
-        }
-        // Log Insert 처리
-        if (true) {
-          const log_insert_query = `INSERT INTO ${log_table} (
-          uid,
-          subscription_log_type,
-          subscription_log_coupon_number,
-          subscription_log_coupon_id
-          ) VALUES (?, ?, ?, ?)`;
-          // console.log(plan_log_insert_query);
-          const log_insert_value = [
-            parsepUid,
-            "coupon",
-            couponNumber,
+        res.status(200).json({
+          message:
+            "이용권 쿠폰 유효성 검증에 성공했습니다! (User Coupon validation Success)",
+          data: {
             coupon_id,
-          ];
-          // console.log(plan_log_insert_value);
-          try {
-            await queryAsync(connection_AI, log_insert_query, log_insert_value);
-            console.log(`Subscription Log Insert Success! - pUid: ${pUid}`);
-            return;
-          } catch (err) {
-            console.error("Error executing query:", err);
-            return res.status(500).json({
-              message: `Server Error: ${err.sqlMessage}`,
-            });
-          }
-        }
+            coupon_number,
+            coupon_subscription_period, // 이용권 기간
+          },
+        });
       }
       // 할인권인 경우
       else {
         console.log(
           `할인권 쿠폰 유효성 검증에 성공했습니다! (User Coupon validation Success) - pUid: ${pUid}`
         );
-        res.status(201).json({
+        res.status(200).json({
           message:
             "할인권 쿠폰 유효성 검증에 성공했습니다! (User Coupon validation Success)",
           data: {
